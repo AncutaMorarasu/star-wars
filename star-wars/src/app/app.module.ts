62e9b1612e93c8b1cb3aa63bfc293a6758d96635
AnnieisOK <morarasu.ancuta@gmail.comgit config --global user.email morarasu.ancuta@gmail.comgit config --global user.email morarasu.ancuta@gmail.com>
2020-12-27 23:35:30 +0200

    routes added, navbar created & styled

--- star-wars/src/app/app.module.ts
+++ star-wars/src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavigationComponent } from './components/navigation/navigation/navigation.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { FilmsPageComponent } from './pages/films-page/films-page.component';
+import { CharactersPageComponent } from './pages/characters-page/characters-page.component';
+import { PlanetsPageComponent } from './pages/planets-page/planets-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    HomePageComponent,
+    FilmsPageComponent,
+    CharactersPageComponent,
+    PlanetsPageComponent
   ],
   imports: [
     BrowserModule,
