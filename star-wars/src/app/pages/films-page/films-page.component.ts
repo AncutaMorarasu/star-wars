d03b042f71de21d831733b0ac959da6f531306fd
AnnieisOK <morarasu.ancuta@gmail.comgit config --global user.email morarasu.ancuta@gmail.comgit config --global user.email morarasu.ancuta@gmail.com>
2021-01-07 23:05:54 +0200

    API from swapi, movie thumbnails created

--- star-wars/src/app/pages/films-page/films-page.component.ts
+++ star-wars/src/app/pages/films-page/films-page.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { filmsImg } from 'src/app/src-api';
 
 @Component({
   selector: 'app-films-page',
   templateUrl: './films-page.component.html',
-  styleUrls: ['./films-page.component.css']
+  styleUrls: ['./films-page.component.css'],
 })
 export class FilmsPageComponent implements OnInit {
-
-  constructor() { }
+  movies = [];
+  constructor() {}
 
   ngOnInit(): void {
+    fetch('https://swapi.dev/api/films/')
+      .then((response) => response.json())
+      .then((result) => {
+        this.movies = result.results;
+        //loop through array of movies, if an episode id matches the id from image source API, a new property is added to movie object
+        //new property is 'image' used for the img src in HTML template
+        this.movies.forEach(function (entry) {
+          for (var k = 0; k < filmsImg.length; k++) {
+            if (filmsImg[k].id == entry.episode_id) {
+              entry['image'] = filmsImg[k].src;
+            }
+          }
+        });
+      });
   }
-
 }
